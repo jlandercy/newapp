097749f8612c2a956a0a7204d7378f31d9deaf8b
Jean Landercy <jeanlandercy@live.com>
2019-04-03 17:41:12 +0000

    Login/Logout working

--- newapp/newapp/settings.py
+++ newapp/newapp/settings.py
@@ -133,3 +133,6 @@ STATIC_URL = '/static/'
 
 # https://simpleisbetterthancomplex.com/tutorial/2018/08/13/how-to-use-bootstrap-4-forms-with-django.html
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# https://wsvincent.com/django-user-authentication-tutorial-login-and-logout/
+LOGIN_REDIRECT_URL = '/'
