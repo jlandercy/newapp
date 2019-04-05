62aba75cf0da1e2b94f207c5d54322d7f383fc6b
Jean Landercy <jeanlandercy@live.com>
2019-04-05 09:34:32 +0000

    Added Read only view for UserSession in Audit

--- newapp/audit/admin.py
+++ newapp/audit/admin.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from .models import UserSession
 
 
+# https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Admin_site
 @admin.register(UserSession)
 class UserSessionAdmin(admin.ModelAdmin):
 
-    list_display = ('id', 'user', 'loggedin', 'loggedout', 'session', 'is_active')
+    list_display = ('id', 'user', 'loggedin', 'loggedout', 'elapsed', 'is_active', 'session')
     list_filter = ('user',)
     search_fields = ('user',)
     ordering = ('-id',)
